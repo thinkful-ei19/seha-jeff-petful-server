9d430816e17f7cb5d156623e16b41396aeedf9b6
Jeff Vega <jeff@intellibliss.com>
2018-05-25 08:39:10 -0700

    working get cat/dog

--- index.js
+++ index.js
@@ -21,6 +21,33 @@ app.use(
     origin: CLIENT_ORIGIN
   })
 );
+const cats = [{
+  imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+  imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+  name: 'Fluffy',
+  sex: 'Female',
+  age: 2,
+  breed: 'Bengal',
+  story: 'Thrown on the street'
+}];
+
+const dogs = [{
+  imageURL: 'http://www.dogster.com/wp-content/uploads/2015/05/Cute%20dog%20listening%20to%20music%201_1.jpg',
+  imageDescription: 'A smiling golden-brown golden retreiver listening to music.',
+  name: 'Zeus',
+  sex: 'Male',
+  age: 3,
+  breed: 'Golden Retriever',
+  story: 'Owner Passed away'
+}];
+app.get('/api/cat', (req, res) => {
+  return res.json(cats[0]);
+});
+
+app.get('/api/dog', (req, res) => {
+  return res.json(dogs[0]);
+});
+
 
 function runServer(port = PORT) {
   const server = app
